4e733eb1a00327a636ee0b34f140c0cd2669aa9e
S Joseph <shmulijo@gmail.com>
2025-07-02 20:24:23 -0400

    Removed unnecessary imports and changed next.config

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-import Banner from "./components/Banner"
 
 export default function RootLayout({ children }) {
   return (
